34e3362214916b786d3380662dc9ad6954d22e23
doubleaykay <the.a.cars@gmail.com>
2021-03-31 16:45:01 -0400

    work with multiple people, understand sys.argv, clean up

--- engs146_file_namer.py
+++ engs146_file_namer.py
@@ -1,3 +1,5 @@
+import sys
+
 # import pyperclip if it exists, if it does not then don't try to use it
 try:
     __import__('pyperclip')
@@ -7,48 +9,83 @@ else:
     import pyperclip
     clpbrd = True
 
-# import pyperclip
+# function to handle logic
+def gen_fname(ss, gg):
+    # format is:
+    # ss-gg-yyt-p-xnn descriptor
 
-# format is:
-# ss-gg-yyt-p-xnn descriptor
+    # ss = student number
+    # ss = '09' # for anoush
 
-# FIXED
-ss = '09' # for anoush
-yyt = '21S'
+    # gg = group number
+    if not gg:
+        gg = input('Enter two digit group number: ')
+        # gg = '00' # for individual stuff
+    
+    # yyt = year and term
+    yyt = '21S'
 
-# NOT FIXED
-# gg
-gg = input('Enter two digit group number: ')
-# gg = '00' # for individual stuff
+    # p = project identifier
+    p_text = \
+    '''Project identifier options:
+        0	No associated project
+        1	CAD Lessons
+        2	3D Printer Mods
+        3	Fidget Spinner
+        4	Chronometer
+    Selection: '''
+    p = input(p_text)
 
-# p
-p_text = \
-'''Project identifier options:
-    0	No associated project
-    1	CAD Lessons
-    2	3D Printer Mods
-    3	Fidget Spinner
-    4	Chronometer
-Selection: '''
-p = input(p_text)
+    # x = drawing identifier
+    x = input('\033[1m\033[4m'+'P'+'\033[0m'+'art or '+'\033[1m\033[4m'+'A'+'\033[0m'+'ssembly? ')
+    # x = 'P' # or 'A' for assembly
 
-# x
-x = input('\033[1m\033[4m'+'P'+'\033[0m'+'art or '+'\033[1m\033[4m'+'A'+'\033[0m'+'ssembly? ')
-# x = 'P' # or 'A' for assembly
+    # nn = part number
+    nn = input('Two digit part number: ')
+    # nn = '01'
 
-# nn
-nn = input('Two digit part number: ')
-# nn = '01'
+    # descriptor
+    descriptor = input('Description: ')
+    # descriptor = 'widget'
 
-# descriptor
-descriptor = input('Description: ')
-# descriptor = 'widget'
+    # generate file name
+    fname = f'{ss}-{gg}-{yyt}-{p}-{x}{nn} {descriptor}'
 
-# generate file name
-fname = f'{ss}-{gg}-{yyt}-{p}-{x}{nn} {descriptor}'
+    # display result, and copy to clipboard if possible
+    if clpbrd:
+        pyperclip.copy(fname)
+        print(f'Filename:\n {fname}\nhas been copied to the clipboard.')
+    else:
+        print(f'Filename:\n {fname}')
 
-if clpbrd:
-    pyperclip.copy(fname)
-    print(f'Filename:\n {fname}\nhas been copied to the clipboard.')
-else:
-    print(f'Filename:\n {fname}')
+# person info dictionaries
+anoush = {
+    'ss': '09',
+    'gg': None
+}
+
+bridger = {
+    'ss': '08',
+    'gg': None
+}
+
+vann = {
+    'ss': '07',
+    'gg': None
+}
+
+# handle running script
+if __name__ == "__main__":
+    if len(sys.argv) == 1:
+        # assume anoush is running script
+        print(f'Running script as anoush')
+        gen_fname(anoush['ss'], anoush['gg'])
+    else:
+        person = sys.argv[-1]
+        print(f'Running script as {person}')
+        if person == 'anoush' or person == 'a':
+            gen_fname(anoush['ss'], anoush['gg'])
+        elif person == 'bridger' or person == 'b':
+            gen_fname(bridger['ss'], bridger['gg'])
+        elif person == 'vann' or person == 'v':
+            gen_fname(vann['ss'], vann['gg'])
